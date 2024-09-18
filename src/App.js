34221cb06cc440462bcc4799a70794da39b2d699
Júllio César <julliocesar5002@gmail.com>
2024-09-18 11:46:54 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,142 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div
+      className="MacbookAir1"
+      style={{
+        width: 1280,
+        height: 832,
+        position: "relative",
+        background: "#F20808",
+      }}
+    >
+      <div
+        className="Rectangle2"
+        style={{
+          width: 271,
+          height: 305,
+          left: 299.24,
+          top: 314.94,
+          position: "absolute",
+          background: "white",
+          boxShadow: "13px 17px 24.899999618530273px 15px rgba(0, 0, 0, 0.25)",
+          borderRadius: 74,
+        }}
+      />
+      <div
+        className="Rectangle3"
+        style={{
+          width: 271,
+          height: 305,
+          left: 624.24,
+          top: 314.94,
+          position: "absolute",
+          background: "white",
+          boxShadow: "13px 17px 24.899999618530273px 15px rgba(0, 0, 0, 0.25)",
+          borderRadius: 74,
+        }}
+      />
+      <div
+        className="Rectangle4"
+        style={{
+          width: 271,
+          height: 305,
+          left: 624.24,
+          top: 314.94,
+          position: "absolute",
+          background: "white",
+          boxShadow: "13px 17px 24.899999618530273px 15px rgba(0, 0, 0, 0.25)",
+          borderRadius: 74,
+        }}
+      />
+      <img
+        className="PngwingCom1"
+        style={{
+          width: 318.44,
+          height: 329.11,
+          left: 192,
+          top: 415.57,
+          position: "absolute",
+          transform: "rotate(-30.28deg)",
+          transformOrigin: "0 0",
+        }}
+        src="https://via.placeholder.com/318x329"
+      />
+      <div
+        className="Ellipse1"
+        style={{
+          width: 289.75,
+          height: 279.07,
+          left: 49.25,
+          top: 22,
+          position: "absolute",
+          background: "#FFD900",
+        }}
+      ></div>
+      <img
+        className="BurgerLogoLogotypeSilhouette8136651"
+        style={{
+          width: 232,
+          height: 220,
+          left: 76,
+          top: 35,
+          position: "absolute",
+        }}
+        src="https://via.placeholder.com/232x220"
+      />
+      <div
+        className="FaASeuPedido"
+        style={{
+          width: 706,
+          height: 97,
+          left: 414,
+          top: 22,
+          position: "absolute",
+          color: "white",
+          fontSize: 100,
+          fontFamily: "Jaro",
+          fontWeight: "400",
+          wordWrap: "break-word",
+        }}
+      >
+        FAÇA SEU PEDIDO!
+      </div>
+      <img
+        className="PngtreeMeltedCheeseWithYellowColorVectorPngImage125273521"
+        style={{
+          width: 231.15,
+          height: 85.26,
+          left: 320,
+          top: 315.53,
+          position: "absolute",
+          borderRadius: 42.6,
+        }}
+        src="https://via.placeholder.com/231x85"
+      />
+      <img
+        className="PngtreeMeltedCheeseWithYellowColorVectorPngImage125273522"
+        style={{
+          width: 231.15,
+          height: 85.26,
+          left: 644,
+          top: 315.51,
+          position: "absolute",
+          borderRadius: 42.6,
+        }}
+        src="https://via.placeholder.com/231x85"
+      />
+      <img
+        className="PngwingCom11"
+        style={{
+          width: 150,
+          height: 295,
+          left: 685.24,
+          top: 314.94,
+          position: "absolute",
+        }}
+        src=""
+      />
+    </div>
+  );
+}
---
+++ src/App2.js
@@ -0,0 +1,217 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div
+      className="MacbookAir2"
+      style={{
+        width: 1280,
+        height: 832,
+        position: "relative",
+        background: "#F20808",
+      }}
+    >
+      <div
+        className="Rectangle6"
+        style={{
+          width: 386.5,
+          height: 832,
+          left: -13,
+          top: 0,
+          position: "absolute",
+        }}
+      ></div>
+      <div
+        className="Ellipse3"
+        style={{
+          width: 841.09,
+          height: 523,
+          left: 538.84,
+          top: -6.68,
+          position: "absolute",
+          transform: "rotate(90.04deg)",
+          transformOrigin: "0 0",
+          background: "white",
+        }}
+      ></div>
+      <div
+        className="Ellipse2"
+        style={{
+          width: 841.09,
+          height: 523,
+          left: 523.52,
+          top: -1.91,
+          position: "absolute",
+          transform: "rotate(90.04deg)",
+          transformOrigin: "0 0",
+          background: "#FFA100",
+        }}
+      ></div>
+      <img
+        className="PngwingCom3"
+        style={{
+          width: 112.32,
+          height: 107.49,
+          left: -2.14,
+          top: 180.19,
+          position: "absolute",
+          transform: "rotate(-32.26deg)",
+          transformOrigin: "0 0",
+        }}
+        src="https://via.placeholder.com/112x107"
+      />
+      <img
+        className="PngwingCom2"
+        style={{
+          width: 177.65,
+          height: 167.55,
+          left: -35,
+          top: 151.82,
+          position: "absolute",
+          transform: "rotate(-32.26deg)",
+          transformOrigin: "0 0",
+        }}
+        src="https://via.placeholder.com/178x168"
+      />
+      <div
+        className="RXxXx"
+        style={{
+          width: 168,
+          height: 63,
+          left: 193,
+          top: 141,
+          position: "absolute",
+          color: "white",
+          fontSize: 50,
+          fontFamily: "Jaro",
+          fontWeight: "400",
+          wordWrap: "break-word",
+        }}
+      >
+        R$ XX,xx
+      </div>
+      <div
+        className="RXxXx"
+        style={{
+          width: 168,
+          height: 63,
+          left: 194,
+          top: 430,
+          position: "absolute",
+          color: "white",
+          fontSize: 50,
+          fontFamily: "Jaro",
+          fontWeight: "400",
+          wordWrap: "break-word",
+        }}
+      >
+        R$ XX,xx
+      </div>
+      <div
+        className="RXxXx"
+        style={{
+          width: 168,
+          height: 63,
+          left: 190,
+          top: 141,
+          position: "absolute",
+          color: "black",
+          fontSize: 50,
+          fontFamily: "Jaro",
+          fontWeight: "400",
+          wordWrap: "break-word",
+        }}
+      >
+        R$ XX,xx
+      </div>
+      <div
+        className="RXxXx"
+        style={{
+          width: 168,
+          height: 63,
+          left: 191,
+          top: 430,
+          position: "absolute",
+          color: "black",
+          fontSize: 50,
+          fontFamily: "Jaro",
+          fontWeight: "400",
+          wordWrap: "break-word",
+        }}
+      >
+        R$ XX,xx
+      </div>
+      <img
+        className="PngwingCom12"
+        style={{
+          width: 140,
+          height: 194,
+          left: 8,
+          top: 311,
+          position: "absolute",
+        }}
+        src="https://via.placeholder.com/140x194"
+      />
+      <img
+        className="PngwingCom13"
+        style={{
+          width: 177.65,
+          height: 246.17,
+          left: 2,
+          top: 269,
+          position: "absolute",
+        }}
+        src="https://via.placeholder.com/178x246"
+      />
+      <img
+        className="Banner0f835336988523795d281"
+        style={{
+          width: 732,
+          height: 574,
+          left: 524,
+          top: 258,
+          position: "absolute",
+        }}
+        src="https://via.placeholder.com/732x574"
+      />
+      <div
+        className="TransformeSuaRefeiOEmUmMomentoEspecial"
+        style={{
+          left: 339,
+          top: 28,
+          position: "absolute",
+          textAlign: "center",
+        }}
+      >
+        <span style="color: 'white', fontSize: 50, fontFamily: 'Jaro', fontWeight: '400', wordWrap: 'break-word'">
+          TRANSFORME SUA REFEIÇÃO EM UM MOMENTO <br />
+        </span>
+        <span style="color: 'white', fontSize: 60, fontFamily: 'Jaro', fontWeight: '400', wordWrap: 'break-word'">
+          ESPECIAL
+        </span>
+      </div>
+      <div
+        className="Ellipse2"
+        style={{
+          width: 289.75,
+          height: 279.07,
+          left: 966,
+          top: 113,
+          position: "absolute",
+          background: "#FFD900",
+        }}
+      ></div>
+      <img
+        className="BurgerLogoLogotypeSilhouette8136652"
+        style={{
+          width: 232,
+          height: 220,
+          left: 992.75,
+          top: 126,
+          position: "absolute",
+        }}
+        src="https://via.placeholder.com/232x220"
+      />
+    </div>
+  );
+}
